8ef4a090f42e5ba74fe14af21162978c68a5f317
Valeri <vakrus@ttu.ee>
2019-04-09 20:22:16 +0300

    converted to async, refactoring

--- routes/api/results.js
+++ routes/api/results.js
@@ -5,130 +5,167 @@ const Result = require('../../models/Result')
 const validateResultFields = require('../../helpers/validateResultFields')
 
 
+//GET RESULT BY ID
+
+router.get('/:id', async (req, res) => {
+  try {
+    const result = await Result.findById(req.params.id)
+    res.json(result)
+  } catch (e) {
+    await res.json({
+      error: 'Something went wrong.'
+    })
+  }
+})
+
 //ADD NEW RESULT
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
   const { errors, isValid } = validateResultFields(req.body)
 
   if (!isValid) {
-    return res.json(400, errors)
+    res.json(400, errors)
   }
 
-  Result.findOne({ athlete: req.body.athlete })
-    .then(result => {
-      if (result) {
-        errors.result = 'This athlete already has a result'
-        return res.json(400, errors)
-      } else {
-        const newResult = new Result({
-          rank: req.body.rank,
-          country: req.body.country,
-          athlete: req.body.athlete,
-          time: req.body.time,
-          points: req.body.points,
-          eventName: req.body.eventName
-        })
-        newResult.save().then(result => res.json(result))
-      }
-    }).catch(err => console.log(err))
+  const result = await Result
+    .findOne({ athlete: req.body.athlete })
+
+  if (result) {
+    errors.result = 'This athlete already has a result'
+    res.json(400, errors)
+  } else {
+    const newResult = new Result({
+      rank: req.body.rank,
+      country: req.body.country,
+      athlete: req.body.athlete,
+      time: req.body.time,
+      points: req.body.points,
+      eventName: req.body.eventName
+    })
+    try {
+      await newResult.save()
+      await res.json(newResult)
+    } catch (e) {
+      await res.json({
+        error: 'Something went wrong.'
+      })
+    }
+  }
 })
+
 
 //GET RESULTS BY EVENTNAME
 
-router.get('/:eventName', (req, res) => {
-  Result.find()
-    .then(results => {
-      const filteredResults = results
-        .filter(result => result.eventName === req.params.eventName)
-      res.json(filteredResults)
-    }).catch(err => console.log(err))
+router.get('/event/:eventName', async (req, res) => {
+  const results = await Result.find()
+  const filteredResults = await results
+    .filter(result => result.eventName === req.params.eventName)
+  try {
+    await res.json(filteredResults)
+  } catch (e) {
+    await res.json({
+      error: 'Something went wrong.'
+    })
+  }
 })
+
 
 //SORT RESULTS BY POINTS ASCENDING
 
-router.get('/filter/asc/:eventName', (req, res) => {
-  Result.find()
+router.get('/filter/asc/:eventName', async (req, res) => {
+  const results = await Result
+    .find()
     .sort({ points: 1 })
-    .then(results => {
-      const filteredResults = results
-        .filter(result => result.eventName === req.params.eventName)
-      res.json(filteredResults)
-    }).catch(err => console.log(err))
+  const filteredResults = await results
+    .filter(result => result.eventName === req.params.eventName)
+  try {
+    await res.json(filteredResults)
+  } catch (e) {
+    await res.json({
+      error: 'Something went wrong.'
+    })
+  }
 })
 
 //SORT RESULTS BY POINTS DESCENDING
 
-router.get('/filter/desc/:eventName', (req, res) => {
-  Result.find()
+router.get('/filter/desc/:eventName', async (req, res) => {
+  const results = await Result
+    .find()
     .sort({ points: -1 })
-    .then(results => {
-      const filteredResults = results
-        .filter(result => result.eventName === req.params.eventName)
-      res.json(filteredResults)
-    }).catch(err => console.log(err))
+  const filteredResults = await results
+    .filter(result => result.eventName === req.params.eventName)
+  try {
+    await res.json(filteredResults)
+  } catch (e) {
+    await res.json({
+      error: 'Something went wrong.'
+    })
+  }
 })
 
 //GET ALL RESULTS
 
-router.get('/', (req, res) => {
-  Result.find()
-    .then(results => res.json(results))
-    .catch(err => console.log(err))
-})
-
-//GET RESULT BY ID
-
-router.get('/:id', (req, res) => {
-  Result.findById(req.params.id)
-    .then(result => {
-      if (!result) {
-        return res.json(404, {
-          error: 'Result does not exist.'
-        })
-      } else {
-        return res.json(result)
-      }
-    }).catch(err => console.log(err))
+router.get('/', async (req, res) => {
+  const results = await Result.find()
+  try {
+    await res.json(results)
+  } catch (e) {
+    await res.json({
+      error: 'Something went wrong.'
+    })
+  }
 })
 
 
 //DELETE RESULT BY ID
 
-router.delete('/:id', (req, res) => {
-  Result.findById(req.params.id)
-    .then(result => {
-      result.remove().then(result => res.json(result))
+router.delete('/:id', async (req, res) => {
+  const result = await Result.findById(req.params.id)
+  try {
+    await result.remove()
+    await res.json(result)
+  } catch (e) {
+    await res.json({
+      error: 'Something went wrong.'
     })
+  }
 })
 
 //UPDATE RESULT 
 
-router.put('/:id', (req, res) => {
-  Result.findByIdAndUpdate(req.params.id)
-    .then(result => {
-      if (!result) {
-        return res.json(404, {
-          err: 'No result found.'
-        })
-      } else {
-        const updatedData = {
-          rank: req.body.rank,
-          country: req.body.country,
-          athlete: req.body.athlete,
-          time: req.body.time,
-          points: req.body.points
-        }
-        const { rank, country, athlete, time, points } = updatedData
-        result.rank = rank
-        result.country = country
-        result.athlete = athlete
-        result.time = time
-        result.points = points
-        result.save().then(res.json(result))
-      }
+router.put('/:id', async (req, res) => {
+  const result = await Result.findByIdAndUpdate(req.params.id)
+  if (!result) {
+    res.json(404, {
+      err: 'No result found.'
     })
+  } else {
+    const updatedData = {
+      rank: req.body.rank,
+      country: req.body.country,
+      athlete: req.body.athlete,
+      time: req.body.time,
+      points: req.body.points
+    }
+    const { rank, country, athlete, time, points } = updatedData
+    result.rank = rank
+    result.country = country
+    result.athlete = athlete
+    result.time = time
+    result.points = points
+    try {
+      await result.save()
+      await res.json(result)
+    } catch (e) {
+      await res.json({
+        error: 'Something went wrong.'
+      })
+    }
+  }
 })
+
 
 
 module.exports = router
