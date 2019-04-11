f94ea9756469124f1e1e05c497827cc29a39f0f5
Valeri <vakrus@ttu.ee>
2019-04-11 18:47:27 +0300

    'ez'

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://valerikrusinin:qwerty123456@ds135456.mlab.com:35456/sportlased'
+  mongoURI: process.env.MONGO_URI
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -17,6 +18,14 @@ const events = require('./routes/api/events')
 
 app.use('/api/results', results)
 app.use('/api/events', events)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 mongoose
   .connect(db)
